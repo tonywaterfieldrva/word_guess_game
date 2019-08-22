024f2dfb74cf441e67444e7cd57c198d782b7bee
Tony Waterfield <tonywaterfieldrva@gmail.com>
2019-08-21 23:58:48 -0400

    updating images, index.html, style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,2 @@
 
-    var targetWords = ["Trout", "Bluefish", "Croaker", "Flounder", "Cobia"];
-   
-    document.onkeyup = function(event) {
-         
-      var userGuess = event.key;
-      var randomWord = targetWords[Math.floor(Math.random() * targetWords.length)];
-
-     alert("User guess: " + userGuess);
-     alert(randomWord);
-    }
+  
